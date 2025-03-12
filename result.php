939a1113ff29a6e272e6b16720a738c0aedab8b7
warunikobbekaduwa <warunikobb2@gmail.com>
2025-03-12 18:51:37 +0530

    add grade column for result sheet

--- result.php
+++ result.php
@@ -23,9 +23,6 @@ include('includes/config.php');
             <div class="content-wrapper">
                 <div class="content-container">
 
-         
-                    <!-- /.left-sidebar -->
-
                     <div class="main-page">
                         <div class="container-fluid">
                             <div class="row page-title-div">
@@ -33,19 +30,11 @@ include('includes/config.php');
                                     <h2 class="title" align="center">Result Management System</h2>
                                 </div>
                             </div>
-                            <!-- /.row -->
-                          
-                            <!-- /.row -->
                         </div>
-                        <!-- /.container-fluid -->
 
                         <section class="section" id="exampl">
                             <div class="container-fluid">
-
                                 <div class="row">
-                              
-                             
-
                                     <div class="col-md-8 col-md-offset-2">
                                         <div class="panel">
                                             <div class="panel-heading">
@@ -53,176 +42,133 @@ include('includes/config.php');
                                                     <h3 align="center">Student Result Details</h3>
                                                     <hr />
 <?php
-// code Student Data
-$rollid=$_POST['rollid'];
-$classid=$_POST['class'];
-$_SESSION['rollid']=$rollid;
-$_SESSION['classid']=$classid;
-$qery = "SELECT   tblstudents.StudentName,tblstudents.RollId,tblstudents.RegDate,tblstudents.StudentId,tblstudents.Status,tblclasses.ClassName,tblclasses.Section from tblstudents join tblclasses on tblclasses.id=tblstudents.ClassId where tblstudents.RollId=:rollid and tblstudents.ClassId=:classid ";
-$stmt = $dbh->prepare($qery);
-$stmt->bindParam(':rollid',$rollid,PDO::PARAM_STR);
-$stmt->bindParam(':classid',$classid,PDO::PARAM_STR);
-$stmt->execute();
-$resultss=$stmt->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($stmt->rowCount() > 0)
-{
-foreach($resultss as $row)
-{   ?>
-<p><b>Student Name :</b> <?php echo htmlentities($row->StudentName);?></p>
-<p><b>Student Roll Id :</b> <?php echo htmlentities($row->RollId);?>
-<p><b>Student Class:</b> <?php echo htmlentities($row->ClassName);?>(<?php echo htmlentities($row->Section);?>)
-<?php }
+// Fetch Student Data
+$rollid = $_POST['rollid'];
+$classid = $_POST['class'];
+$_SESSION['rollid'] = $rollid;
+$_SESSION['classid'] = $classid;
 
-    ?>
+$qery = "SELECT tblstudents.StudentName, tblstudents.RollId, tblstudents.RegDate, tblstudents.StudentId, tblstudents.Status, tblclasses.ClassName, tblclasses.Section 
+         FROM tblstudents 
+         JOIN tblclasses ON tblclasses.id = tblstudents.ClassId 
+         WHERE tblstudents.RollId = :rollid AND tblstudents.ClassId = :classid";
+
+$stmt = $dbh->prepare($qery);
+$stmt->bindParam(':rollid', $rollid, PDO::PARAM_STR);
+$stmt->bindParam(':classid', $classid, PDO::PARAM_STR);
+$stmt->execute();
+$resultss = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+if ($stmt->rowCount() > 0) {
+    foreach ($resultss as $row) { ?>
+        <p><b>Student Name :</b> <?php echo htmlentities($row->StudentName); ?></p>
+        <p><b>Student Roll Id :</b> <?php echo htmlentities($row->RollId); ?></p>
+        <p><b>Student Class:</b> <?php echo htmlentities($row->ClassName); ?> (<?php echo htmlentities($row->Section); ?>)</p>
+    <?php }
+}
+?>
                                             </div>
                                             <div class="panel-body p-20">
-
-
-
-
-
-
-
                                                 <table class="table table-hover table-bordered" border="1" width="100%">
-                                                <thead>
+                                                    <thead>
                                                         <tr style="text-align: center">
                                                             <th style="text-align: center">#</th>
-                                                            <th style="text-align: center"> Subject</th>    
+                                                            <th style="text-align: center">Subject</th>    
                                                             <th style="text-align: center">Marks</th>
+                                                            <th style="text-align: center">Grade</th>
                                                         </tr>
-                                               </thead>
-  
-
-
-                                                	
-                                                	<tbody>
-<?php                                              
-// Code for result
-
- $query ="select t.StudentName,t.RollId,t.ClassId,t.marks,SubjectId,tblsubjects.SubjectName from (select sts.StudentName,sts.RollId,sts.ClassId,tr.marks,SubjectId from tblstudents as sts join  tblresult as tr on tr.StudentId=sts.StudentId) as t join tblsubjects on tblsubjects.id=t.SubjectId where (t.RollId=:rollid and t.ClassId=:classid)";
-$query= $dbh -> prepare($query);
-$query->bindParam(':rollid',$rollid,PDO::PARAM_STR);
-$query->bindParam(':classid',$classid,PDO::PARAM_STR);
-$query-> execute();  
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($countrow=$query->rowCount()>0)
-{ 
-foreach($results as $result){
-
-    ?>
-
-                                                		<tr>
-<th scope="row" style="text-align: center"><?php echo htmlentities($cnt);?></th>
-<td style="text-align: center"><?php echo htmlentities($result->SubjectName);?></td>
-<td style="text-align: center"><?php echo htmlentities($totalmarks=$result->marks);?></td>
-                                                		</tr>
-<?php 
-$totlcount+=$totalmarks;
-$cnt++;}
-?>
-<tr>
-<th scope="row" colspan="2" style="text-align: center">Total Marks</th>
-<td style="text-align: center"><b><?php echo htmlentities($totlcount); ?></b> out of <b><?php echo htmlentities($outof=($cnt-1)*100); ?></b></td>
-                                                        </tr>
-<tr>
-<th scope="row" colspan="2" style="text-align: center">Percntage</th>           
-<td style="text-align: center"><b><?php echo  htmlentities($totlcount*(100)/$outof); ?> %</b></td>
-</tr>
-
-<tr>
-                              
-<td colspan="3" align="center"><i class="fa fa-print fa-2x" aria-hidden="true" style="cursor:pointer" OnClick="CallPrint(this.value)" ></i></td>
-                                                             </tr>
-
- <?php } else { ?>     
-<div class="alert alert-warning left-icon-alert" role="alert">
-                                            <strong>Notice!</strong> Your result not declare yet
- <?php }
-?>
-                                        </div>
- <?php 
- } else
- {?>
-
-<div class="alert alert-danger left-icon-alert" role="alert">
-strong>Oh snap!</strong>
+                                                    </thead>
+                                                    <tbody>
 <?php
-echo htmlentities("Invalid Roll Id");
- }
+// Fetch Result Data
+$query = "SELECT t.StudentName, t.RollId, t.ClassId, t.marks, SubjectId, tblsubjects.SubjectName 
+          FROM (SELECT sts.StudentName, sts.RollId, sts.ClassId, tr.marks, SubjectId 
+                FROM tblstudents AS sts 
+                JOIN tblresult AS tr ON tr.StudentId = sts.StudentId) AS t 
+          JOIN tblsubjects ON tblsubjects.id = t.SubjectId 
+          WHERE (t.RollId = :rollid AND t.ClassId = :classid)";
+
+$query = $dbh->prepare($query);
+$query->bindParam(':rollid', $rollid, PDO::PARAM_STR);
+$query->bindParam(':classid', $classid, PDO::PARAM_STR);
+$query->execute();
+$results = $query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt = 1;
+$totlcount = 0;
+
+if ($query->rowCount() > 0) {
+    foreach ($results as $result) {
+        $marks = $result->marks;
+
+        // Grading logic
+        if ($marks >= 75) {
+            $grade = "A";
+        } elseif ($marks >= 65) {
+            $grade = "B";
+        } elseif ($marks >= 45) {
+            $grade = "C";
+        } elseif ($marks >= 35) {
+            $grade = "S";
+        } else {
+            $grade = "F";
+        }
 ?>
-                                        </div>
-
-
-
-                                                	</tbody>
+        <tr>
+            <th scope="row" style="text-align: center"><?php echo htmlentities($cnt); ?></th>
+            <td style="text-align: center"><?php echo htmlentities($result->SubjectName); ?></td>
+            <td style="text-align: center"><?php echo htmlentities($marks); ?></td>
+            <td style="text-align: center"><?php echo htmlentities($grade); ?></td>
+        </tr>
+<?php
+        $totlcount += $marks;
+        $cnt++;
+    }
+?>
+        <tr>
+            <th scope="row" colspan="2" style="text-align: center">Total Marks</th>
+            <td style="text-align: center"><b><?php echo htmlentities($totlcount); ?></b> out of <b><?php echo htmlentities(($cnt - 1) * 100); ?></b></td>
+        </tr>
+        <tr>
+            <th scope="row" colspan="2" style="text-align: center">Percentage</th>           
+            <td style="text-align: center"><b><?php echo htmlentities($totlcount * 100 / (($cnt - 1) * 100)); ?> %</b></td>
+        </tr>
+        <tr>
+            <td colspan="3" align="center">
+                <i class="fa fa-print fa-2x" aria-hidden="true" style="cursor:pointer" onClick="CallPrint()"></i>
+            </td>
+        </tr>
+<?php
+} else { ?>
+        <div class="alert alert-warning left-icon-alert" role="alert">
+            <strong>Notice!</strong> Your result has not been declared yet.
+        </div>
+<?php } ?>
+                                                    </tbody>
                                                 </table>
-
                                             </div>
                                         </div>
-                                        <!-- /.panel -->
                                     </div>
-                                    <!-- /.col-md-6 -->
-
-                                    <div class="form-group">
-                                                           
-                                                            <div class="col-sm-6">
-                                                               <a href="index.php">Back to Home</a>
-                                                            </div>
-                                                        </div>
-
                                 </div>
-                                <!-- /.row -->
-  
                             </div>
-                            <!-- /.container-fluid -->
                         </section>
-                        <!-- /.section -->
-
                     </div>
-                    <!-- /.main-page -->
 
-                  
                 </div>
-                <!-- /.content-container -->
             </div>
-            <!-- /.content-wrapper -->
-
         </div>
-        <!-- /.main-wrapper -->
 
-        <!-- ========== COMMON JS FILES ========== -->
         <script src="js/jquery/jquery-2.2.4.min.js"></script>
         <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/pace/pace.min.js"></script>
-        <script src="js/lobipanel/lobipanel.min.js"></script>
-        <script src="js/iscroll/iscroll.js"></script>
-
-        <!-- ========== PAGE JS FILES ========== -->
-        <script src="js/prism/prism.js"></script>
-
-        <!-- ========== THEME JS ========== -->
-        <script src="js/main.js"></script>
         <script>
-            $(function($) {
-
-            });
-
-
-            function CallPrint(strid) {
-var prtContent = document.getElementById("exampl");
-var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
-WinPrint.document.write(prtContent.innerHTML);
-WinPrint.document.close();
-WinPrint.focus();
-WinPrint.print();
-}
-</script>
-
+            function CallPrint() {
+                var prtContent = document.getElementById("exampl");
+                var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+                WinPrint.document.write(prtContent.innerHTML);
+                WinPrint.document.close();
+                WinPrint.focus();
+                WinPrint.print();
+            }
         </script>
-
-        <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
-
     </body>
 </html>
